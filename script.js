7285a41bb7eee12a81a18e05f9ca474ecb4cc4c2
Daniel Oliveira <daniel.oliveira.com>
2022-01-16 12:32:36 -0300

    implantar traduzirCondicao

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
